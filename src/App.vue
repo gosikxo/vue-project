0c0e8cd4b4415c5ef001d9831f6bf3c1862017d9
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-10-04 17:36:51 +0300

    Vue directives

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
-  const greeting = 'hello'
+  let greeting = 'hello'
+  const isVisible = true
+  const isVisible2 = false
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" v-cloak>
     <h1> {{greeting}} </h1>
+    <input v-model="greeting"/>
+    <div class="box" v-if="isVisible"></div>
+    <div class="box2" v-else-if="isVisible2"></div>
+    <div class="box3" v-else></div>
   </div>
 </template>
+
+<style>
+  .box {
+    background-color: red;
+    width: 40px;
+    height: 40px;
+  }
+  .box2 {
+    background-color: blue;
+    width: 40px;
+    height: 40px;
+  }
+  .box3 {
+    background-color: pink;
+    width: 40px;
+    height: 40px;
+  }
+  [v-cloak] {
+    display: none
+  }
+</style>
